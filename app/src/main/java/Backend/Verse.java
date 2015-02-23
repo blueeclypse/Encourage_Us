a5ac82336a0118edd190c86ed99fcd20d5d2e9a4
Hoang <yamatoeclypse@gmail.com>
2015-02-23 13:06:20 -0500

    Initial commit

---
+++ app/src/main/java/Backend/Verse.java
@@ -0,0 +1,28 @@
+package Backend;
+
+/**
+ * Created by Hoang on 2/23/2015.
+ */
+public class Verse {
+    private String verse = ""; //stores the verse
+    private int book; //book number(chronological order)
+    private int chapter; //chapter number
+    private int verseNum; //verse number
+
+    public Verse(String verse, int book, int chapter, int verseNum){
+        this.verse = verse;
+        this.book = book;
+        this.chapter = chapter;
+        this.verseNum = verseNum;
+    }
+
+    //returns the verse
+    public String getVerse(){
+        return verse;
+    }
+
+    //unfinished
+    public String getReference(){
+        return book+"";
+    }
+}
